8d249be8b61e1d51efd70cca4bbca8c2ce534257
Anna Rachel Valdez <raachann@Anna-Rachel.attlocal.net>
2020-05-14 23:31:44 -0700

    added routes for lobby and game page

--- my-app/routes/index.js
+++ my-app/routes/index.js
@@ -6,13 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
 router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Express' });
+});
+
+router.get('/lobby', function(req, res, next) {
+  res.render('lobby', { title: 'Lobby Page' });
+});
+
+router.get('/game', function(req, res, next) {
+  res.render('game', { title: 'Game Page' });
 });
 
 
