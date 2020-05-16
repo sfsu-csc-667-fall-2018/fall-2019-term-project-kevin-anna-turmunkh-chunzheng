74ed4b40c34f97aade3d65cb2ef7c7bce2c3b8cd
Anna Rachel Valdez <raachann@Anna-Rachel.attlocal.net>
2020-05-16 13:21:05 -0700

    game

--- my-app/public/javascripts/board.js
+++ my-app/public/javascripts/board.js
@@ -1,7 +1,9 @@
 var config = {
-    position: 'start',
-    draggable: true
-  }
+  draggable: true,
+  dropOffBoard: 'snapback',
+  position: 'start'
   
-  
-  var board1 = ChessBoard('board', config);
+}
+
+var ruyLopez = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
+var board = ChessBoard('board', config);
